0c26048029b54fea9f6a84ef81ed8335a41f779a
Rijo George <rijogeorge@rijos-mbp-2.lan>
2022-02-16 18:45:54 -0800

    done

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,9 +1,10 @@
-import React from 'react';
-
 const Pokecard = (props) => (
-    <>
-    hello
-    </>
+    <div>
+    <h1> {props.data.name} </h1>
+    <p> <img src = {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.data.id}.png`} /></p>
+    <p> Type: {props.data.type} </p>
+    <p> EXP: {props.data.base_experience} </p>
+    </div>
 )
 
 export default Pokecard
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,9 +1,23 @@
 import Pokecard from './Pokecard'
 
-const Pokedex = () => (
-    <>
-    <Pokecard />
-    </>
-)
+const data = [
+    {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
+    {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
+    {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
+    {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
+    {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
+    {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
+    {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
+    {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
+  ]
+const Pokedex = () => {
+    
+    let pokecardList = []
+    for (let d of data) {
+      pokecardList.push(<li><Pokecard data={d} /></li>)
+    }
+    return <ul>{pokecardList}</ul>
+    
+}
 
 export default Pokedex
